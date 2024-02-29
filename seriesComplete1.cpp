2fdb5ae5b0ae7a2ba27b2e0efa290941f57763d3
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-29 22:19:14 +0300

    seriesComplete1
    we coded series example in this project wit c++

---
+++ seriesComplete1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+int series1[10];
+int series2[]={90,80,70,55,61};
+int series3[5]={90,80,70,55,61};
+
+/* cout<<series2[3]<<endl;
+printf("%d",series2[3]);
+
+ series[0]=1;
+ series1[1]=2;
+ series[2]=3;
+ cout<<series[0];   */	
+ 
+ cout<<series3[5];
+ return 0;
+	
+}
